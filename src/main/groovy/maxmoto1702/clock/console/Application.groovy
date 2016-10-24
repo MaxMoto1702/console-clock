8e7ee582833764c21df65542ab63bf25e446055e
Max Serebryanskiy <maxim.serebryanskiy@icloud.com>
2016-10-24 19:37:33 +0300

    fix sync

--- src/main/groovy/maxmoto1702/clock/console/Application.groovy
+++ src/main/groovy/maxmoto1702/clock/console/Application.groovy
@@ -4,6 +4,7 @@ import maxmoto1702.clock.console.renderers.ConsoleRenderer
 
 class Application {
     static void main(String... args) {
+        sleep(System.currentTimeMillis() % 60_000)
         def renderer = new ConsoleRenderer()
         while (true) {
             renderer.render(Calendar.getInstance())
